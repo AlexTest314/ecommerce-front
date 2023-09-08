f30273d57b8aa06781145ddb524e4e8bc938cf7b
Alextest2 <olepry98@gmail.com>
2023-09-08 19:07:33 +0300

    change method api

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -9,31 +9,33 @@ const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
 
 const handler = async (req, res) => {
   await mongooseConnect();
-  const buf = await buffer(req);
-  const sig = req.headers["stripe-signature"];
+  if (req.method === "POST") {
+    const buf = await buffer(req);
+    const sig = req.headers["stripe-signature"];
 
-  let event;
+    let event;
 
-  try {
-    event = stripe.webhooks.constructEvent(buf.toString(), sig, webhookSecret);
-  } catch (err) {
-    res.status(400).send(`Webhook Error: ${err.message}`);
-    return;
-  }
+    try {
+      event = stripe.webhooks.constructEvent(buf.toString(), sig, webhookSecret);
+    } catch (err) {
+      res.status(400).send(`Webhook Error: ${err.message}`);
+      return;
+    }
 
-  // Handle the event
-  switch (event.type) {
-    case "checkout.session.completed":
-      const data = event.data.object;
-      const orderId = data.metadata.orderId;
-      const paid = data.payment_status === "paid";
-      if (orderId && paid) {
-        await Order.findByIdAndUpdate(orderId, { paid: true });
-      }
-      // Then define and call a function to handle the event payment_intent.succeeded
-      break;
-    default:
-      console.log(`Unhandled event type ${event.type}`);
+    // Handle the event
+    switch (event.type) {
+      case "checkout.session.completed":
+        const data = event.data.object;
+        const orderId = data.metadata.orderId;
+        const paid = data.payment_status === "paid";
+        if (orderId && paid) {
+          await Order.findByIdAndUpdate(orderId, { paid: true });
+        }
+        // Then define and call a function to handle the event payment_intent.succeeded
+        break;
+      default:
+        console.log(`Unhandled event type ${event.type}`);
+    }
   }
   res.status(200).send("ok");
 };
@@ -41,13 +43,5 @@ const handler = async (req, res) => {
 export const config = {
   api: { bodyParser: false }
 };
-
-//pamper-glow-eases-wows
-
-//grace-amply-fame-remedy
-
-//acct_1No2ICLp65SVTFdB
-
-//stripe listen --forward-to localhost:3000/api/webhook
 
 export default handler;
