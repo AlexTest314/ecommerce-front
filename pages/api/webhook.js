1f70b53b0fe9414ec99886208d32f7a3bc94ca22
Alextest2 <olepry98@gmail.com>
2023-09-08 21:56:23 +0300

    stripe change

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -12,42 +12,36 @@ const cors = Cors({
 
 const webhookHandler = async (req, res) => {
   await mongooseConnect();
-  if (req.method === "POST") {
-    const buf = await buffer(req);
-    const sig = req.headers["stripe-signature"];
 
-    let event;
+  const buf = await buffer(req);
+  const sig = req.headers["stripe-signature"];
 
-    try {
-      event = stripe.webhooks.constructEvent(buf.toString(), sig, webhookSecret);
-    } catch (err) {
-      console.log(`❌ Error message: ${err.message}`);
-      res.status(400).send(`Webhook Error: ${err.message}`);
-      return;
-    }
+  let event;
 
-    console.log("✅ Success:", event.id);
-
-    // Handle the event
-    try {
-      switch (event.type) {
-        case "checkout.session.completed":
-          const data = event.data;
-          const orderId = data.metadata.orderId;
-          const paid = data.payment_status === "paid";
-          if (orderId && paid) {
-            await Order.findByIdAndUpdate(orderId, { paid: true });
-            await Order.findByIdAndRemove("64fb6bb2a0c09d0a3350f812");
-          }
-          // Then define and call a function to handle the event payment_intent.succeeded
-          break;
-        default:
-          console.log(`Unhandled event type ${event.type}`);
-      }
-    } catch (error) {
-      console.log(error.message);
-    }
+  try {
+    event = stripe.webhooks.constructEvent(buf.toString(), sig, webhookSecret);
+  } catch (err) {
+    res.status(400).send(`Webhook Error: ${err.message}`);
+    return;
   }
+
+  // Handle the event
+
+  switch (event.type) {
+    case "checkout.session.completed":
+      const data = event.data;
+      const orderId = data.metadata.orderId;
+      const paid = data.payment_status === "paid";
+      if (orderId && paid) {
+        await Order.findByIdAndUpdate(orderId, { paid: true });
+        await Order.findByIdAndRemove("64fb6bb2a0c09d0a3350f812");
+      }
+      // Then define and call a function to handle the event payment_intent.succeeded
+      break;
+    default:
+      console.log(`Unhandled event type ${event.type}`);
+  }
+
   res.status(200).send("ok");
 };
 
