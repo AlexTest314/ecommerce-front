19fd8ae6c1c3c57b1b7167418a611d939e4110ed
Alextest2 <olepry98@gmail.com>
2023-09-09 08:00:27 +0300

    stripe change

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -25,9 +25,42 @@ const webhookHandler = async (req, res) => {
     return;
   }
 
+  const permittedEvents = ["checkout.session.completed", "payment_intent.succeeded", "payment_intent.payment_failed"];
+
+  if (permittedEvents.includes(event.type)) {
+    let data;
+
+    try {
+      switch (event.type) {
+        case "checkout.session.completed":
+          const data = event.data;
+          const orderId = data.metadata.orderId;
+          const paid = data.payment_status === "paid";
+          if (orderId && paid) {
+            await Order.findByIdAndUpdate(orderId, { paid: true });
+          }
+          console.log(`💰 CheckoutSession status: ${data.payment_status}`);
+          break;
+        case "payment_intent.failed":
+          data = event.data.object;
+          console.log(`❌ Payment failed: ${data.last_payment_error?.message}`);
+          break;
+        case "payment_intent.succeeded":
+          data = event.data.object;
+          console.log(`💰 PaymentIntent status: ${data.status}`);
+          break;
+        default:
+          throw new Error(`Unhhandled event: ${event.type}`);
+      }
+    } catch (error) {
+      console.log(error);
+      return NextResponse.json({ message: "Webhook handler failed" }, { status: 500 });
+    }
+  }
+
   // Handle the event
 
-  switch (event.type) {
+  /*   switch (event.type) {
     case "checkout.session.completed":
       const data = event.data;
       const orderId = data.metadata.orderId;
@@ -42,8 +75,8 @@ const webhookHandler = async (req, res) => {
   }
 
   res.status(200).send("ok");
+}; */
 };
-
 export const config = {
   api: { bodyParser: false }
 };
